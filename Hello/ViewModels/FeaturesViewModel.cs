fa72208b57c30a9db41630e4e34a3ac731efd2d2
Damien Touchette <djtouchette1993@gmail.com>
2017-03-08 20:04:40 -0700

    Initial commit

---
+++ Hello/ViewModels/FeaturesViewModel.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Hello.Forms.ViewModels
+{
+	public class FeaturesViewModel : Screen
+	{
+		private string textChange = "hello";
+		private int counter = 0;
+
+		public string TextChange
+		{
+			get { return textChange; }
+			set
+			{
+				textChange = value;
+				NotifyOfPropertyChange(() => TextChange);
+			}
+		}
+
+		public int Counter
+		{
+			get { return counter; }
+			set
+			{
+				counter = value;
+				NotifyOfPropertyChange(() => Counter);
+			}
+		}
+
+		public void Increment()
+		{
+			Counter++;
+		}
+
+		public void ChangeText()
+		{
+			TextChange = "Look I changed man MVVM Power.";
+		}
+
+
+	}
+}
